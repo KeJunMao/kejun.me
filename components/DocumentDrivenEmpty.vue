685988d8112e40a5f83c7f4ebddc8488c1390b75
kejun <kejun1997@gmail.com>
2023-08-23 21:05:31 +0800

    feat: add better empty

--- components/DocumentDrivenEmpty.vue
+++ components/DocumentDrivenEmpty.vue
@@ -1,3 +1,8 @@
 <template>
-  <p>empty :(</p>
+  <div my-6xl flex flex-col items-center justify-center op-70>
+    <div i-ri-inbox-2-fill text-8xl />
+    <div mt-2 text-sm>
+      这里什么都没有
+    </div>
+  </div>
 </template>
